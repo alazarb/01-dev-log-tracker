1a17b50ede8cb727a28798af010bd6251373f06d
Alazar <alazarb133@gmail.com>
2026-07-22 13:52:28 +0300

    Add JavaScript basics and user interaction

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Day 9 - JavaScript Basics
+
+let userName = prompt("What is your name?");
+
+alert("Welcome " + userName + "!");
+
+console.log("User Name:", userName);
+
+let currentDay = 9;
+console.log("Bootcamp Day:", currentDay);
+
+const bootcamp = "60-Day Full-Stack Bootcamp";
+console.log(bootcamp);
